6ae07686ff2529bceca506bc152e341c3d7a498a
jhy <492468308@qq.com>
2019-03-03 00:07:05 +0800

    'fix bug'

--- Django/settings.py
+++ Django/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'tfzxxwr)y9(0%lgt!uwkb-77%bv_o9a%bkp!r8-w%g!zld-5ho'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
